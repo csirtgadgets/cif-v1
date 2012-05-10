db47ff6ef61230f75f0fa88084dde596d0d02ec6
Wes Young <wes@barely3am.com>
2012-05-10 19:46:01 +0000

    adding

---
+++ perl/lib/CIF/Msg.pm
@@ -0,0 +1,46 @@
+package CIF::Msg;
+use base 'CIF::_Msg';
+
+use 5.008008;
+use strict;
+use warnings;
+
+our $VERSION = '0.00.01';
+$VERSION = eval $VERSION;  # see L<perlmodstyle>
+
+1;
+__END__
+
+=head1 NAME
+
+Iodef::Pb - Perl extension for generating IODEF protocol buffer objects
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head2 EXPORT
+
+None by default. Object Oriented.
+
+=head1 SEE ALSO
+
+ http://github.com/collectiveintel/cif-protocol
+ http://collectiveintel.net
+
+=head1 AUTHOR
+
+Wes Young, E<lt>wes@barely3am.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+  Copyright (C) 2012 by Wes Young <wesyoung.me>
+  Copyright (C) 2012 the REN-ISAC <ren-isac.net>
+  Copyright (C) 2012 the trustee's of Indiana University <iu.edu>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.1 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
