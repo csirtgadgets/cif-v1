b0f12233f63427a10b57a14a23583251b7360b40
Kevin Benton <blak111@gmail.com>
2012-06-14 22:10:04 +0000

    fix to prevent Gzip.pm from returning non-text from gzip archives (aka tar files)

--- lib/CIF/Smrt/Plugin/Decode/Gzip.pm
+++ lib/CIF/Smrt/Plugin/Decode/Gzip.pm
@@ -1,4 +1,4 @@
-package CIF::Smrt::Plugin::Decode::Gzip;
+package CIF::FeedParser::Plugin::Decode::Gzip;
 
 use Compress::Zlib;
 
@@ -7,7 +7,14 @@ sub decode {
     my $data = shift;
     my $type = shift;
     return unless($type =~ /gzip/);
-    return Compress::Zlib::memGunzip($data);
+
+    my $uncompressed = Compress::Zlib::memGunzip($data);
+    my $ft = File::Type->new();
+    my $t = $ft->mime_type($uncompressed);
+
+    return unless($t =~ /octet-stream/); #only return octect streams(aka text)
+    return $uncompressed;
 }
 
 1;
+
