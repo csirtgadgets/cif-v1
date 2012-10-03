60620b9dc26849dc2711021703db52b387b10322
Wes Young <wes@barely3am.com>
2012-10-03 13:13:35 +0000

    ...

--- schemas/Pg/index/hash.sql
+++ schemas/Pg/index/hash.sql
@@ -1,10 +1,10 @@
-SET default_tablespace = 'index';
+CREATE INDEX idx_hash_uuipace = 'index';
 
 DROP INDEX IF EXISTS idx_hash_md5;
-CREATE INDEX idx_hash_md5 ON hash_md5 (detecttime DESC, confidence DESC, guid);
+CREATE INDEX idx_hash_md5 ON hash_md5 (hash, detecttime DESC, confidence DESC, guid);
 
 DROP INDEX IF EXISTS idx_hash_sha1;
-CREATE INDEX idx_hash_sha1 ON hash_sha1 (detecttime DESC, confidence DESC, guid);
+CREATE INDEX idx_hash_sha1 ON hash_sha1 (hash, detecttime DESC, confidence DESC, guid);
 
 DROP INDEX IF EXISTS idx_hash_uuid;
-CREATE INDEX idx_hash_uuid ON hash_uuid (detecttime DESC, confidence DESC, guid);
+CREATE INDEX idx_hash_uuid ON hash_uuid (hash, detecttime DESC, confidence DESC, guid);ON hash_uuid (detecttime DESC, confidence DESC, guid);
