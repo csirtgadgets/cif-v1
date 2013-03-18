658a89e3e1d3c04830762e8e6892893091a7cadf
Wes Young <wes@barely3am.com>
2013-03-18 12:11:39 +0000

    adding lc check to addresses

---
+++ lib/CIF/Smrt/Plugin/Preprocessor/Address.pm
@@ -0,0 +1,16 @@
+package CIF::Smrt::Plugin::Preprocessor::Address;
+
+use strict;
+use warnings;
+
+sub process {
+    my $class = shift;
+    my $rules = shift;
+    my $rec = shift;
+    
+    return unless($rec->{'address'});
+    $rec->{'address'} = lc($rec->{'address'});
+    return $rec;
+}
+
+1;
