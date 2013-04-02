daeeb680f00067451dd425610b8244a6a0e53f97
Wes Young <wes@barely3am.com>
2013-04-02 14:13:00 +0000

    ...

--- lib/CIF.pm
+++ lib/CIF.pm
@@ -164,18 +164,25 @@ sub generate_uuid_url {
 =cut
 
 sub normalize_timestamp {
-    my $dt = shift;
-    return $dt if($dt =~ /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/);
+    my $dt  = shift;
+    my $now = shift || DateTime->from_epoch(epoch => time()); # better perf in loops if we can pass the default now value
+    
+    return DateTime::Format::DateParse->parse_datetime($dt) if($dt =~ /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/);
+    
+    # already epoch
+    return DateTime->from_epoch(epoch => $dt) if($dt =~ /^\d{10}$/);
+    
+    # something else
     if($dt && ref($dt) ne 'DateTime'){
         if($dt =~ /^\d+$/){
             if($dt =~ /^\d{8}$/){
                 $dt.= 'T00:00:00Z';
                 $dt = eval { DateTime::Format::DateParse->parse_datetime($dt) };
                 unless($dt){
-                    $dt = DateTime->from_epoch(epoch => time());
+                    $dt = $now;
                 }
             } else {
-                $dt = DateTime->from_epoch(epoch => $dt);
+                $dt = $now;
             }
         } elsif($dt =~ /^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\S+)?$/) {
             my ($year,$month,$day,$hour,$min,$sec,$tz) = ($1,$2,$3,$4,$5,$6,$7);
@@ -186,7 +193,6 @@ sub normalize_timestamp {
             return undef unless($dt);
         }
     }
-    $dt = $dt->ymd().'T'.$dt->hms().'Z';
     return $dt;
 }
 
